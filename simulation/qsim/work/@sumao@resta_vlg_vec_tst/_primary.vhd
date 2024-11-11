97f698ce073e6639eeba6dfb1037ba0324ae3309
ManuelPCabrera <131972714+ManuelPCabrera@users.noreply.github.com>
2024-11-11 16:17:55 -0300

    Delete simulation directory
    ""

--- simulation/qsim/work/@sumao@resta_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity SumaoResta_vlg_vec_tst is
-end SumaoResta_vlg_vec_tst;
--- simulation/qsim/work/ubicacionparte2v2_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ubicacionparte2v2_vlg_vec_tst is
-end ubicacionparte2v2_vlg_vec_tst;
